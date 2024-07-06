ef94a248e523a72185b722eb4a1fb8e8548e421e
shiho-k <shii0327@yahoo.co.jp>
2024-07-06 23:30:08 +0900

    課題提出です。

---
+++ src/BaseBallTeam/Review03.java
@@ -0,0 +1,78 @@
+package BaseBallTeam;
+
+public class Review03 {
+
+    public static void main(String[] args) {
+
+        BaseBallTeam baseballteam1=new BaseBallTeam();
+        baseballteam1.setName("東京ヤクルトスワローズ");
+        baseballteam1.setWin(80);
+        baseballteam1.setLose(59);
+        baseballteam1.setDraw(4);
+        baseballteam1.report();
+        BaseBallTeam baseballteam2=new BaseBallTeam();
+        baseballteam2.setName("横浜DeNAベイスターズ");
+        baseballteam2.setWin(73);
+        baseballteam2.setLose(68);
+        baseballteam2.setDraw(2);
+        baseballteam2.report();
+        BaseBallTeam baseballteam3=new BaseBallTeam();
+        baseballteam3.setName("阪神タイガース");
+        baseballteam3.setWin(68);
+        baseballteam3.setLose(71);
+        baseballteam3.setDraw(4);
+        baseballteam3.report();
+        BaseBallTeam baseballteam4=new BaseBallTeam();
+        baseballteam4.setName("読売ジャイアンツ");
+        baseballteam4.setWin(68);
+        baseballteam4.setLose(72);
+        baseballteam4.setDraw(3);
+        baseballteam4.report();
+        BaseBallTeam baseballteam5=new BaseBallTeam();
+        baseballteam5.setName("広島東洋カープ");
+        baseballteam5.setWin(66);
+        baseballteam5.setLose(74);
+        baseballteam5.setDraw(3);
+        baseballteam5.report();
+        BaseBallTeam baseballteam6=new BaseBallTeam();
+        baseballteam6.setName("中日ドラゴンズ");
+        baseballteam6.setWin(66);
+        baseballteam6.setLose(75);
+        baseballteam6.setDraw(2);
+        baseballteam6.report();
+
+        String baseballteamName1=baseballteam1.getName();
+        int baseballteamWin1=baseballteam1.getWin();
+        int baseballteamLose1=baseballteam1.getLose();
+        int baseballteamDraw1=baseballteam1.getDraw();
+
+        String baseballteamName2=baseballteam2.getName();
+        int baseballteamWin2=baseballteam2.getWin();
+        int baseballteamLose2=baseballteam2.getLose();
+        int baseballteamDraw2=baseballteam2.getDraw();
+
+        String baseballteamName3=baseballteam3.getName();
+        int baseballteamWin3=baseballteam3.getWin();
+        int baseballteamLose3=baseballteam3.getLose();
+        int baseballteamDraw3=baseballteam3.getDraw();
+
+        String baseballteamName4=baseballteam4.getName();
+        int baseballteamWin4=baseballteam4.getWin();
+        int baseballteamLose4=baseballteam4.getLose();
+        int baseballteamDraw4=baseballteam4.getDraw();
+
+        String baseballteamName5=baseballteam5.getName();
+        int baseballteamWin5=baseballteam5.getWin();
+        int baseballteamLose5=baseballteam5.getLose();
+        int baseballteamDraw5=baseballteam5.getDraw();
+
+        String baseballteamName6=baseballteam6.getName();
+        int baseballteamWin6=baseballteam6.getWin();
+        int baseballteamLose6=baseballteam6.getLose();
+        int baseballteamDraw6=baseballteam6.getDraw();
+
+
+
+}
+
+}
